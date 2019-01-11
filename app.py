5a0c74e792346f6ff6c8cb354cb2e52513676fa6
matteoferla <matteo.ferla@gmail.com>
2019-01-11 11:16:40 +0000

    :wrench: changing port to not clash

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from pyramid.paster import get_app
 
 # custom `app.py` made to use instead of using `pserve` as I am not running a venv atm.
 app = get_app('production.ini', 'main')
-serve(app, host='0.0.0.0', port=8080, threads=50)
+serve(app, host='0.0.0.0', port=8088, threads=50)
