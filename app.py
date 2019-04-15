988078dcda8739b8dfab46ea0a97e2c928a3ad8e
matteoferla <matteo.ferla@gmail.com>
2019-04-15 17:13:07 +0100

    :bug: env variable issue

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 # custom `app.py` made to use instead of using `pserve` as I am not running a venv atm.
 
 if 'SQL_URL' in os.environ:  # postgres in production!
-    app = get_app('production.ini', 'main', sql_url=os.environ['SQL_URL'])
+    app = get_app('production.ini', 'main', options={'sql_url':os.environ['SQL_URL']})
 else:
     app = get_app('development.ini', 'main')
 serve(app, host='0.0.0.0', port=8088, threads=50)
