48b17a35a81b34593e05b337c2f7649f19a9a01d
Matteo Ferla <matteo.ferla@gmail.com>
2019-08-19 11:41:36 +0100

    :beetle: hilarious bug, fixed. selection must be within edit div: $('#edit_description')[0].contains(document.getSelection().getRangeAt(0).commonAncestorContainer)

--- michelanglo_app/templates/edit_modal/edit_modal.js
+++ michelanglo_app/templates/edit_modal/edit_modal.js
@@ -186,7 +186,8 @@ $('#usespan').click(function () {
     }
 
     window.currentRange.deleteContents();
-    window.currentRange.insertNode( $('<span>'+addenda+'</span>')[0] );
+    let span = '<span>'+addenda+'</span>';
+    $('#edit_description')[0].contains(document.getSelection().getRangeAt(0).commonAncestorContainer) ? window.currentRange.insertNode( $(span)[0] ) : $('#edit_description').prepend(span);
     //let d = $('#edit_description');
     //d.html(d.html()+'\n'+addenda);
     //$('[data-toggle="tooltip"]').tooltip();
