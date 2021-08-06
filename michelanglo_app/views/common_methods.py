b3d6379550a3f1557e9c539dae1c52068ff21aae
matteoferla <matteo.ferla@gmail.com>
2021-08-06 13:43:06 +0100

    :hammer: check if species is AlphaFold2 OK

--- michelanglo_app/views/common_methods.py
+++ michelanglo_app/views/common_methods.py
@@ -362,3 +362,33 @@ def get_history(request):
         code = get_pdb_code(request)
         history = {'code': code, 'changes': ''}
     return history
+
+alphamodels = {'Arabidopsis thaliana': 3702,
+                     'Caenorhabditis elegans': 6239,
+                     'Candida albicans': 5476,
+                     'Danio rerio': 7955,
+                     'Dictyostelium discoideum': 44689,
+                     'Drosophila melanogaster': 7227,
+                     'Escherichia coli': 562,
+                     'Glycine max': 3847,
+                     'Homo sapiens': 9606,
+                     'Leishmania infantum': 5671,
+                     'Methanocaldococcus jannaschii': 243232,
+                     'Mus musculus': 10090,
+                     'Mycobacterium tuberculosis': 1773,
+                     'Oryza sativa': 4530,
+                     'Plasmodium falciparum': 5833,
+                     'Rattus norvegicus': 10116,
+                     'Saccharomyces cerevisiae': 4932,
+                     'Schizosaccharomyces pombe': 4896,
+                     'Staphylococcus aureus': 1280,
+                     'Trypanosoma cruzi': 5693}
+
+def is_alphafolded(taxid:int) -> bool:
+    try:
+        if int(taxid) in alphamodels.values():
+            return True
+        else:
+            return False
+    except Exception:
+        return False
