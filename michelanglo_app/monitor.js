3de83caab1b3a2679a7ababca99c8780bc0e52d0
matteoferla <matteo.ferla@gmail.com>
2021-10-20 16:41:11 +0100

    :hammer: fixed the puppeteer issue —NGL.download blob was not being passed

--- michelanglo_app/monitor.js
+++ michelanglo_app/monitor.js
@@ -1,21 +1,43 @@
+/*
+This nodejs script loads and saves the image to make a monitoring images
+requires PORT and USER_DATA env variables and the uuid to check as argument
+*/
+
 process.on('unhandledRejection', up => { throw up });
-//required!
-// if it crashes and complains about no sandbox try `export PUPPETEER_CHROME=/bin/chromium-browser` for dev'ing
+process.on('UnhandledPromiseRejectionWarning', err => process.exit(1) ); //this does nothing.
+
 const puppeteer = require('puppeteer');
-const fs = require('fs');
+const fse = require('fs-extra');
+const timeout = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 const uuid = process.argv[2];
 const prefix = process.argv.length === 4 ? process.argv[3] : '';
-const timeout = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-//console.log('Loaded');
-//console.log(`Checking links for ${uuid}`);
+console.log(`Parsing ${uuid}`);
 
 (async () => {
-    //process.env.PUPPETEER_CHROME ? await puppeteer.launch({executablePath: process.env.PUPPETEER_CHROME}) : ...
-    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-    const page = await browser.newPage();
-    const url = `http://localhost:8088/data/${uuid}?columns_viewport=6`;
+    //for some reason I cannot get npm to install chromium on centos.
+  const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox',
+                                                                        '--disable-setuid-sandbox',
+                                                                        '--disable-web-security']});
+   //{headless: false} if dev.
+  const page = await browser.newPage();
+  await page.setViewport({
+                          width: 1000,
+                          height: 700,
+                          deviceScaleFactor: 1,
+                        });
+  page
+    .on('console', message =>
+      console.log(`${message.type().substr(0, 3).toUpperCase()} ${message.text()}`))
+    .on('pageerror', ({ message }) => console.log(message))
+    .on('response', response =>
+      console.log(`${response.status()} ${response.url()}`))
+    .on('requestfailed', request =>
+      console.log(`${request.failure().errorText} ${request.url()}`));
+
+    //await page.setRequestInterception(true);
+    //page.on('request', (request) => ...); // block requests?
+    await page.goto(`http://localhost:${process.env.PORT}/data/${uuid}?columns_viewport=6`);
     let labels = ['Initial view'];
-    await page.goto(url);
     //conf.
     await page.setViewport({
                           width: 1000,
@@ -25,7 +47,7 @@ const timeout = (ms) => new Promise(resolve => setTimeout(resolve, ms));
     await page.click('body');
     await page.evaluate( () => $('#viewport img').click() ? $('#viewport img').length : undefined );
     await page._client.send('Page.setDownloadBehavior', {behavior: 'allow',
-                                                         downloadPath: './michelanglo_app/user-data-monitor/'});
+                                                         downloadPath: `${process.env.USER_DATA}/monitor/`});
 
 
     //navigation lock
@@ -40,18 +62,36 @@ const timeout = (ms) => new Promise(resolve => setTimeout(resolve, ms));
     });
     await page.setRequestInterception(true);
     await timeout(4000); //safe side//
-    // start clicking!
+    // links
     const nLinks = await page.evaluate( () => $('.prolink').length );
-    const saver = (fn) => NGL.getStage().makeImage({trim: true, antialias: true, transparent: false}).then((blob) => NGL.download(blob, fn));
-    await page.evaluate(saver,`${prefix}${uuid}-0.png`);
+    // open:
+    const saver = async(filename) => {
+        const response = await page.evaluate(() => new Promise((resolve, reject) => {
+          NGL.getStage()
+              .makeImage({trim: true, antialias: false, transparent: false})
+              .then((blob) => {
+                  const reader = new FileReader();
+                  reader.readAsBinaryString(blob);
+                  reader.onload = () => resolve(reader.result);
+                  reader.onerror = () => reject('Error occurred while reading binary string');
+              })
+          })
+        );
+        const file = Buffer.from(response , 'binary');
+        fse.writeFileSync(filename, file);
+    };
+    await saver(`${prefix}${process.env.USER_DATA}/monitor/${uuid}-0.png`);
+
+    // start clicking!
     for (let i=0; i < Math.min(nLinks, 100); i++) {
         await page.evaluate((index) => $(`.prolink:eq(${index})`).click(), i);
         labels.push(await page.evaluate((index) => $(`.prolink:eq(${index})`).text(), i));
         //console.log(labels[i]);
         await timeout(4000); //safe side//
-        await page.evaluate(saver, `${prefix}${uuid}-${i+1}.png`);
+        await saver(`${prefix}${process.env.USER_DATA}/monitor/${uuid}-${i+1}.png`);
     }
-  await timeout(4000); //safe side//
   await browser.close();
-  fs.writeFile(`./michelanglo_app/user-data-monitor/${prefix}${uuid}.json`, JSON.stringify(labels), 'utf8', err => undefined);
+  fse.writeFileSync(`${process.env.USER_DATA}/monitor/${prefix}${uuid}.json`,
+                JSON.stringify(labels),
+                'utf8');
 })();
--- michelanglo_app/thumbnail.js
+++ michelanglo_app/thumbnail.js
@@ -1,21 +1,27 @@
 /*
 This nodejs script loads and saves the image to make a thumbnail for twitter & co..
+requires PORT and USER_DATA env variables and the uuid to check as argument
 */
 
 process.on('unhandledRejection', up => { throw up });
+process.on('UnhandledPromiseRejectionWarning', err => process.exit(1) ); //this does nothing.
 
 const puppeteer = require('puppeteer');
-
-const uuid = process.argv[2];
-
+const fse = require('fs-extra');
 const timeout = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+const uuid = process.argv[2];
+console.log(`Parsing ${uuid}`);
 
 
-process.on('UnhandledPromiseRejectionWarning', err => process.exit(1) ); //this does nothing.
+
+
+
 
 (async () => {
   //for some reason I cannot get npm to install chromium on centos.
-  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox',
+                                                                        '--disable-setuid-sandbox',
+                                                                        '--disable-web-security']});
    //{headless: false} if dev.
   const page = await browser.newPage();
   await page.setViewport({
@@ -23,55 +29,36 @@ process.on('UnhandledPromiseRejectionWarning', err => process.exit(1) ); //this 
                           height: 700,
                           deviceScaleFactor: 1,
                         });
-  await page.goto(`http://localhost:8088/data/${uuid}?columns_viewport=12`);
+  page
+    .on('console', message =>
+      console.log(`${message.type().substr(0, 3).toUpperCase()} ${message.text()}`))
+    .on('pageerror', ({ message }) => console.log(message))
+    .on('response', response =>
+      console.log(`${response.status()} ${response.url()}`))
+    .on('requestfailed', request =>
+      console.log(`${request.failure().errorText} ${request.url()}`));
+
+    //await page.setRequestInterception(true);
+    //page.on('request', (request) => ...); // block requests?
+
+  await page.goto(`http://localhost:${process.env.PORT}/data/${uuid}?columns_viewport=12`);
   //const errorLogger = (err) =>  console.log("Internal error: " + err.toString());
   //page.on("pageerror", errorLogger);
   //page.on("error", errorLogger);
   await page.click('body');
   await timeout(3000); //safe side.
-  await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: './michelanglo_app/user-data-thumb/'});
-  let blob = await page.evaluate(
-				(uuid) => NGL.getStage()
-					     .makeImage( {trim: true, antialias: true, transparent: false})
-					     .then((blob) => NGL.download(blob, `${uuid}.png`))
-				, uuid);
-  await timeout(2000);
+  const response = await page.evaluate(() => new Promise((resolve, reject) => {
+          NGL.getStage()
+              .makeImage({trim: true, antialias: false, transparent: false})
+              .then((blob) => {
+                  const reader = new FileReader();
+                  reader.readAsBinaryString(blob);
+                  reader.onload = () => resolve(reader.result);
+                  reader.onerror = () => reject('Error occurred while reading binary string');
+              })
+          })
+  );
+    const file = Buffer.from(response , 'binary');
+    fse.writeFileSync(`${process.env.USER_DATA}/thumb/${uuid}.png`, file);
   await browser.close();
 })();
-
-
-
-// JUNK!
-
-//const NGL = require('ngl'); cannot run headlessly.
-//const fs = require('fs');
-
-  //await page.waitForNavigation({waitUntil: 'domcontentloaded'}); //https://github.com/GoogleChrome/puppeteer/issues/3338
-  //const navigationPromise = page.waitForNavigation({ waitUntil: 'load' });
-
-
-  //await navigationPromise;
-
-/*
-  let blob = await page.evaluate( async () => await NGL.getStage().makeImage( {trim: true, antialias: true, transparent: false})
-				  )
-  console.log(JSON.stringify(blob)); //{}
-  await timeout(1000);
-  console.log(blob.constructor.name); // "object"   
-  //console.log([Blob, Promise].filter((cls) => blob instanceof cls)[0].name) //https://stackoverflow.com/questions/14653349/node-js-can%C2%B4t-create-blobs
-  console.log(JSON.stringify(blob)); //{}
-   
-   //await  fs.writeFile('NGL.png', new Buffer(blob, 'base64'));
-*/
-
-/*
-  
-  //await page.screenshot({path: 'example.png'});
-  const blob = await page.evaluate(()=> NGL.getStage().makeImage( {trim: true, antialias: true, transparent: false}));
-  console.log(blob);
-  console.log(await page.evaluate(()=> NGL.getStage()));
-  await  fs.writeFile('NGL.png', new Buffer(blob, 'base64'));
-  //console.log(blob);
-  await browser.close();
-*/
-
