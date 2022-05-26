3f324b97690aae10472b52884eefb1eb3c4aaef7
matteoferla <matteo.ferla@gmail.com>
2022-05-26 20:02:05 +0100

    :fire: useless file

--- test.py
+++
@@ -1,6 +0,0 @@
-from michelanglo_app.transplier import PyMolTranspiler
-
-transpiler = PyMolTranspiler(file='michelanglo_app/demo/1gfl.pse')
-#print(transpiler.get_view())
-#print(transpiler.get_reps())
-print(transpiler.get_loadfun_js(tag_wrapped=True, viewport='viewport', funname='loadfun'))
